7d7ada206771bad0edcd9301f1e89c8e621e0c07
it8866 <160612311+liit8866@users.noreply.github.com>
2024-06-18 20:01:41 +0800

    Add files via upload
    12

---
+++ upload2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=gb2312" />
+<title>上传插件</title>
+</head>
+<?php
+header('Content-Type: text/html; charset=gb2312'); 
+if(isset($_FILES['file2'])){
+  $file = $_FILES['file2'];
+ 
+  // 保存文件到指定目录
+  move_uploaded_file($file['tmp_name'], './web/2kb.png');
+  echo '文件上传成功！';
+}
+?>
+
+<body>
+</body>
+</html>
+
